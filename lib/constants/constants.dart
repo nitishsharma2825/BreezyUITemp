2a2fdd7b3d458a23eee759618f76123ff2f057ad
ShiKuMi <49393139+shikumi-0@users.noreply.github.com>
2020-09-03 19:26:34 +0530

    Create constants.dart
    made constants

---
+++ lib/constants/constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+//color
+
+const Color kPrimaryColor = Color.fromRGBO(121, 0, 255, 1);
+const Color kAccentColor = Color.fromRGBO(255, 204, 0, 1);
+
+//FontFamily
+
+const String kFontFamilyB = 'Avenir';
+const String kFontFamilyL = 'AvenirLT';
+
+// TextStyle
+const TextStyle kbodyText1 =
+    TextStyle(color: Colors.white, fontSize: 25, fontFamily: kFontFamilyB);
+const TextStyle kbodyText2 = TextStyle(
+  color: Colors.black,
+  fontSize: 18,
+  fontFamily: kFontFamilyL,
+);
